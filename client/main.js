116031b858a5432b94a49e71012c5774a6b5f80b
Nediljko Krstic <nediljko@extensionengine.com>
2019-09-02 21:17:10 +0200

    Import router to main.js

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import App from './App';
+import router from './router';
 import Vue from 'vue';
 
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
