0b86561b07a9a3995da08507e45f9ec93bd597e6
Nediljko Krstic <nediljko@extensionengine.com>
2019-09-02 19:49:15 +0200

    Add simple poi config

---
+++ poi.config.js
@@ -0,0 +1,19 @@
+const extensions = ['.vue'];
+
+const devServer = {
+  headers: {
+    'X-Powered-By': 'Webpack DevSever'
+  },
+  port: 8081
+};
+
+module.exports = {
+  plugins: ['@poi/eslint'],
+  entry: {
+    app: 'client/main.js'
+ },
+  chainWebpack(config) {
+    config.resolve.extensions.merge(extensions);
+  },
+  devServer
+};
