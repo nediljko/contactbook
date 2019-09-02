7fe0b0d29c5eee3b41b70ab0558348ac5cac78b1
Nediljko Krstic <nediljko@extensionengine.com>
2019-09-02 19:47:18 +0200

    Add initial App.vue

---
+++ client/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+#app {
+  height: 100%;
+  overflow-y: scroll;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
