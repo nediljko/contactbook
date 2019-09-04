fc26ab8a9e38ab25bebdc533afe68ffefbc32561
Nediljko Krstic <nediljko@extensionengine.com>
2019-09-04 18:18:35 +0200

    Import and instantiate store in main.js

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
 import App from './App';
 import router from './router';
+import store from './store';
 import Vue from 'vue';
 
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 });
